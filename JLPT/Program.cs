e4544149ff01d9586d96f64edcc8996be90a648d
mruaini <melaniar@hotmail.com>
2023-02-22 18:15:07 -0700

    working on PopulateMasterData

--- JLPT/Program.cs
+++ JLPT/Program.cs
@@ -1,8 +1,11 @@
+using JLPT;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +18,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+ConfigurationManager configuration = builder.Configuration;
+string sqlConnectionString = configuration.GetConnectionString("jlpt");
+builder.Services.AddDbContext<JLPTDbContext>(options => options.UseSqlServer(sqlConnectionString));
 
 app.UseHttpsRedirection();
 
